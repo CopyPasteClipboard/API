5db480f3e9eaa5664c518748af837e4376951ca3
baileympearson <bailey.pearson@vanderbilt.edu>
2018-11-18 13:40:56 -0600

    added routes for boarditem - not tested

--- lib/api_web/controllers/item_controller.ex
+++ lib/api_web/controllers/item_controller.ex
@@ -1,12 +1,29 @@
 defmodule ApiWeb.ItemController do
     use ApiWeb, :controller
-  
-    def getItem(conn, _params) do
-        json conn, ["getItem"]
+
+    defmodule NotFoundError do
+        defexception message: "not found", plug_status: 404
     end
 
-    def deleteItem(conn, _params) do
-        json conn, ["deleteItem"]
+    def get_item(conn, params) do
+        id = params["itemID"]
+
+        item = Api.BoardItem.Queries.get_item_by_id(id)
+
+        if !item do
+            raise(NotFoundError)
+        end
+
+        conn
+            |> put_status(:ok)
+            |> json(%{ :itemid => id, :boarditem => item })
     end
-  
+
+    def deleteItem(conn, params) do
+        id = params["itemID"]
+
+        # status = Api.BoardItem.Queries.delete_item_by_id(id)
+
+        send_resp(conn, :no_content, "")
+    end
 end
